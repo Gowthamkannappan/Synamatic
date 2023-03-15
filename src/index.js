fa24f1d9193686f7481a8f376669f4e21104e3ef
Gowthamkannappan <gowtham@techis.io>
2023-03-16 00:24:31 +0530

    Test

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import createStore from './reducks/store/store';
 import App from './App';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'connected-react-router';
 import reportWebVitals from './reportWebVitals';
+import * as History from 'history';
+import { BrowserRouter } from 'react-router-dom';
 
+const history = History.createBrowserHistory();
+export const store = createStore(history);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+   <Provider store={store}>
+    <BrowserRouter >
+
+            <App />
+
+        </BrowserRouter>
+    </Provider>
+ 
 );
 
 // If you want to start measuring performance in your app, pass a function
